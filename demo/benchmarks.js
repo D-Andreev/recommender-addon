2c346fc4115de770db48a803c982b4cd6bfc6166
Dimitar Andreev <d.andreev90@gmail.com>
2017-05-21 23:40:04 +0300

    fix benchmarks

--- demo/benchmarks.js
+++ demo/benchmarks.js
@@ -5,30 +5,38 @@ var bench = require('fastbench');
 var recommender = require('recommender');
 var documentPath = "./resources/document.txt";
 var documentsPath = "./resources/documents.txt";
-var longDocumentPath = "./resources/longDocument.txt";
-var longDocumentsPath = "./resources/longDocuments.txt";
 var userRatings =  [
 	[ 4, 0, 0, 1, 1, 0, 0 ],
     [ 5, 5, 4, 0, 0, 0, 0 ],
     [ 0, 0, 0, 2, 4, 5, 0 ],
-    [ 0, 3, 0, 0, 0, 0, 3 ]
+    [ 3, 0, 0, 0, 0, 0, 3 ]
 ]
 
 var run = bench([
     function tfidf(done) {
-        var sortedDocs = recommender.tfidf(longDocumentPath, longDocumentsPath);
-        done();
+        recommender.tfidf(documentPath, documentsPath, (sortedDocs) => {
+            assert.equal(sortedDocs.length, 4);
+            done();
+        });
     },
     function ratingPrediction(done) {
-        var predictedRating = recommender.getRatingPrediction(userRatings, 0, 1);
-        assert.ok(predictedRating === 5);
-        done();
+        recommender.getRatingPrediction(userRatings, 0, 1, (predictedRating) => {
+            assert.equal(predictedRating, 5);
+            done();
+        });
     },
     function getGlobalBaselineRatingPrediction(done) {
-        var predictedRating = recommender.getGlobalBaselineRatingPrediction(userRatings, 0, 1);
-        assert.ok(predictedRating === 2.6363636363636362);
-        done();
+        recommender.getGlobalBaselineRatingPrediction(userRatings, 0, 1, (predictedRating) => {
+            assert.equal(predictedRating, 3.6363636363636362);
+            done();
+        });
+    },
+    function getTopCFRecommendations(done) {
+        recommender.getTopCFRecommendations(userRatings, 0, (recommendations) => {
+            assert.equal(recommendations.length, 3);
+            done();
+        });
     }
-], 10);
+], 100000);
 
 run(run);
